383b8a52842e7777caea4bc3e296b9232365bae8
uni <unijeje2@gmail.com>
2026-04-11 18:29:30 +0200

    try to inject gstreamer into workflow

--- lvnm/game_runner.py
+++ lvnm/game_runner.py
@@ -574,12 +574,14 @@ class GameRunner:
                 
                 # Tell GStreamer exactly where its plugins are
                 self.env["GST_PLUGIN_SYSTEM_PATH_1_0"] = gst_plugin_path
+                self.env["GST_PLUGIN_PATH"] = gst_plugin_path 
                 self.env["GST_PLUGIN_SCANNER"] = gst_scanner
                 
                 # Disable the shared registry to avoid "Blacklist" contamination from the host
                 self.env["GST_REGISTRY"] = os.path.join(self.prefix_info["path"], "gst_registry.bin")
                 # Force rescan
                 self.env["GST_REGISTRY_FORK"] = "no"
+                self.env["GST_REGISTRY_UPDATE"] = "yes"
                 
                 logger.debug(f"GStreamer environment sealed to AppImage: {base_lib}")
     
