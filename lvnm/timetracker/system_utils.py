7b749a1b551b3eb37b9d739697a3d6c58a461390
uni <unijeje2@gmail.com>
2026-05-14 21:12:39 +0200

    Add debug logs to wine pid check

--- lvnm/timetracker/system_utils.py
+++ lvnm/timetracker/system_utils.py
@@ -16,7 +16,7 @@ class SystemUtils:
     def is_wine_or_proton(pid):
         try:
             env_result = SystemUtils.get_process_environ(pid)
-            # print(f'env_result: {env_result}')          
+            logger.debug(f"is_wine_or_proton env_result {env_result}")
             return ".exe" in env_result
         except Exception as e:
             logger.error(f"[ERROR] is_wine_or_proton failed: {e}")
@@ -36,9 +36,11 @@ class SystemUtils:
                     title = utils.get_window_name(wid)
                     if not title:
                         continue
-
+                    
+                    logger.debug(f"get_window_list title {title}")
                     if only_show_wine:
                         pid_str = utils.get_window_pid(wid)
+                        logger.debug(f"get_window_list pid_str {pid_str}")
                         if pid_str and pid_str.isdigit() and SystemUtils.is_wine_or_proton(int(pid_str)):
                             window_list.append((title, wid))
                     else:
